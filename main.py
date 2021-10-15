4b9c6e3e82d8ed9d99e61fe71d5a30db37888cd1
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-16 02:59:51 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#           PYTHON LIST COMPREHENSION
+#ordinary syntax:
+numbers = []
+for x in range(10):
+    numbers.append(x)
+print(numbers)
+
+#LIST COMPREHENSION syntax:
+numbers = [x for x in range(10)]
+print(numbers)
+
+#Both get the same result.
+
+#Check the official documentation:
+'''
+https://www.w3schools.com/python/python_lists_comprehension.asp
+'''
